971e83aa4560eef7e146a80776269e3da17d3914
Shivam-Vitkare <120474507+ShivamVitkare@users.noreply.github.com>
2024-11-23 14:42:33 +0530

    first commit

---
+++ src/components/House.js
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+
+function House() {
+  const [btnVisible, setBtnVisible] = useState(false);
+  const [inputVal, setInputVal] = useState(10);
+  const [level, setLevel] = useState(0);
+  const [cost, setCost] = useState(5);
+  const [rent, setRent] = useState(2);
+  const [progress, setProgress] = useState(0); 
+
+  function handleAdd() {
+    if (inputVal >= cost) {
+      setLevel((prev) => prev + 1);
+      setCost((prev) => prev + 0.25);
+      setBtnVisible(true);
+      setInputVal(inputVal - cost);
+    } else {
+      alert("low cost");
+    }
+  }
+
+  function handleRent() {
+    setProgress(0); 
+
+    const fillProgress = setInterval(() => {
+      setProgress((prev) => {
+        if (prev < 100) {
+          return prev + 10; 
+        } else {
+          clearInterval(fillProgress); 
+          setInputVal((prevInput) => prevInput + rent); 
+                    setProgress(0); 
+          return prev; 
+        }
+      });
+    }, 200); 
+
+    return () => clearInterval(fillProgress);
+  }
+
+  function handleManage() {
+    if (inputVal >= rent * 20) {
+      const interval = setInterval(() => {
+        setInputVal((prev) => prev + rent);
+      }, 3000);
+
+      return () => clearInterval(interval);
+    }
+    
+
+    
+
+  }
+
+
+  
+  return (
+    <div>
+      <input
+        style={{ marginBottom: "10px" }}
+        type="number"
+        readOnly
+        value={inputVal}
+        placeholder="Input value"
+      />
+      <button>Save</button>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          border: "1px solid green",
+          width: "60%",
+          margin: "0 auto",
+          padding: "20px",
+        }}
+      >
+        <div>
+          <img
+            style={{ width: "70px", height: "70px" }}
+            src="https://react2eb4af6.onrender.com/imgs/00.png"
+            alt="Studio"
+          />
+          <p>Studio Apartment</p>
+        </div>
+        <div>
+          <div style={{ display: "flex", justifyContent: "space-around" }}>
+            <p>Cost: {cost.toFixed(2)}, </p>
+            <p>Level: {level}, </p>
+            <p>Rent: {rent}, </p>
+            <p>Time: 3</p>
+          </div>
+          <div>
+            
+            <div
+              style={{
+                width: "100%",
+                height: "10px",
+                backgroundColor: "gray",
+                borderRadius: "5px",
+                overflow: "hidden",
+                marginTop: "10px",
+              }}
+            >
+              <div
+                style={{
+                  height: "100%",
+                  width: `${progress}%`,
+                  backgroundColor: "green",
+                  transition: "width 0.2s ease-out",
+                }}
+              ></div>
+            </div>
+          </div>
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          <button onClick={handleAdd}>Buy</button>
+          {btnVisible && (
+            <>
+              <button onClick={handleRent}> Rent</button>
+              <button onClick={handleManage}> Management</button>
+            </>
+          )}
+        </div>
+      </div>
+
+      <br />
+    </div>
+  );
+}
+
+export default House;
